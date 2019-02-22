ff5bbf5965d258ebef782d5ac1c0806062cdf914
Matthew Wright <matthewbwright@gmail.com>
2019-02-21 17:09:43 -0700

    adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: 'Matthew Wright - Web Developer',
+    author: 'Matthew Wright',
+    description: 'Web Developer Portfolio for Matthew Wright',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -15,10 +15,10 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/webdev.jpg', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
   ],
 }
